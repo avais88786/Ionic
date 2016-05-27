e3b76256fbc41700823abb1f44cdda02ba8fc8c5
Avais <avais.mohammad@outlook.com>
2016-05-27 17:03:29 +0100

    Friday

--- www/lib/ionic/js/ionic.bundle.js
+++ www/lib/ionic/js/ionic.bundle.js
@@ -30974,7 +30974,7 @@ function adjustMatchers(matchers) {
  *
  * - your app is hosted at url `http://myapp.example.com/`
  * - but some of your templates are hosted on other domains you control such as
- *   `http://srv01.assets.example.com/`,  `http://srv02.assets.example.com/`, etc.
+ *   `http://srv01.assets.example.com/`,  `http://srv02.assets.example.com/`, etc.
  * - and you have an open redirect at `http://myapp.example.com/clickThru?...`.
  *
  * Here is what a secure configuration for this scenario might look like:
@@ -53169,7 +53169,7 @@ angular.module('ui.router.state')
 
 (function() {
 /* eslint no-unused-vars:0 */
-var IonicModule = angular.module('ionic', ['ngAnimate', 'ngSanitize', 'ui.router', 'ngIOS9UIWebViewPatch'],'ionic.contrib.drawer.vertical'),
+var IonicModule = angular.module('ionic', ['ngAnimate', 'ngSanitize', 'ui.router', 'ngIOS9UIWebViewPatch']),
   extend = angular.extend,
   forEach = angular.forEach,
   isDefined = angular.isDefined,
